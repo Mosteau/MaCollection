4a0ce22794578996bb9409ea00623b1a1a5aff68
Thibaut <thibaut.mosteau@lilo.org>
2024-02-10 13:02:42 +0100

    10-02-24

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -72,16 +72,6 @@ function Home() {
     listGames();
   }, [user]);
 
-  // Suppression d'un jeu vidéo
-  const handleDeleteGames = async (id) => {
-    try {
-      await axios.delete(`${import.meta.env.VITE_BACKEND_URL}/api/games/${id}`);
-      listGames();
-    } catch (error) {
-      console.error("erreur lors de la suppression du jeu vidéo", error);
-    }
-  };
-
   // Ajout d'un jeu vidéo
   const addGame = async (event) => {
     event.preventDefault();
@@ -100,8 +90,19 @@ function Home() {
       });
       const addedGame = { ...gameWithUser, id: response.data.id };
       setGames([...games, addedGame]);
+      listGames();
     } catch (error) {
       console.error("erreur lors de l'ajout du jeu vidéo", error);
+    }
+  };
+
+  // Suppression d'un jeu vidéo
+  const handleDeleteGames = async (id) => {
+    try {
+      await axios.delete(`${import.meta.env.VITE_BACKEND_URL}/api/games/${id}`);
+      listGames();
+    } catch (error) {
+      console.error("erreur lors de la suppression du jeu vidéo", error);
     }
   };
 
@@ -146,6 +147,16 @@ function Home() {
     setSearchTerm(event.target.value);
   };
 
+  // Gestion de la plateforme
+  const handlePlatformChange = (event) => {
+    setNewGame({ ...newGame, platform: event.target.value });
+  };
+
+  // Gestion du genre
+  const handleGenreChange = (event) => {
+    setNewGame({ ...newGame, genre: event.target.value });
+  };
+
   const filteredGames = games.filter((ga) =>
     ga.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -188,29 +199,132 @@ function Home() {
           </div>
           <div className="form-group">
             <label htmlFor="platform">Plateforme</label>
-            <input
-              type="text"
-              className="form-control"
-              value={newGame.platform}
-              onChange={handleInputChange}
+            <select
               id="platform"
               name="platform"
-              placeholder="..."
-              required
-            />
+              value={newGame.platform}
+              onChange={handlePlatformChange}
+            >
+              <option className="button-create-games">
+                Sélectionnez une plateforme
+              </option>
+              <option value="pc" className="button-create-games">
+                PC
+              </option>
+              <option value="ps5" className="button-create-games">
+                PlayStation 5
+              </option>
+              <option value="ps4" className="button-create-games">
+                PlayStation 4
+              </option>
+              <option value="ps3" className="button-create-games">
+                PlayStation 3
+              </option>
+              <option value="ps2" className="button-create-games">
+                PlayStation 2
+              </option>
+              <option value="ps1" className="button-create-games">
+                PlayStation 1
+              </option>
+              <option value="xboxone" className="button-create-games">
+                Xbox One
+              </option>
+              <option value="xbox360" className="button-create-games">
+                Xbox 360
+              </option>
+              <option value="xbox" className="button-create-games">
+                Xbox
+              </option>
+              <option value="nintendo" className="button-create-games">
+                Nintendo
+              </option>
+              <option value="nintendo64" className="button-create-games">
+                Nintendo 64
+              </option>
+              <option value="nintendo3ds" className="button-create-games">
+                Nintendo 3DS
+              </option>
+              <option value="nintendoswitch" className="button-create-games">
+                Nintendo Switch
+              </option>
+              <option value="nintendowii" className="button-create-games">
+                Nintendo Wii
+              </option>
+              <option value="nintendowiiu" className="button-create-games">
+                Nintendo Wii U
+              </option>
+              <option value="nintendogamecube" className="button-create-games">
+                Nintendo GameCube
+              </option>
+              <option value="supernintendo" className="button-create-games">
+                Super Nintendo
+              </option>
+              <option
+                value="nintendoentertainmentsystem"
+                className="button-create-games"
+              >
+                Nintendo NES
+              </option>
+              <option value="dreamcast" className="button-create-games">
+                Dreamcast
+              </option>
+              <option value="sega" className="button-create-games">
+                Sega
+              </option>
+              <option value="atari" className="button-create-games">
+                Atari
+              </option>
+              <option value="autre" className="button-create-games">
+                Autre
+              </option>
+            </select>
           </div>
           <div className="form-group">
             <label htmlFor="genre">Genre</label>
-            <input
-              type="text"
-              className="form-control"
-              value={newGame.genre}
-              onChange={handleInputChange}
+            <select
+              className="select-genre"
               id="genre"
               name="genre"
-              placeholder="..."
-              required
-            />
+              value={newGame.genre}
+              onChange={handleGenreChange}
+            >
+              <option value="" className="button-create-games">
+                Sélectionnez un genre
+              </option>
+              <option value="action" className="button-create-games">
+                Action
+              </option>
+              <option value="aventure" className="button-create-games">
+                Aventure
+              </option>
+              <option value="combat" className="button-create-games">
+                Combat
+              </option>
+              <option value="course" className="button-create-games">
+                Course
+              </option>
+              <option value="plateforme" className="button-create-games">
+                Plateforme
+              </option>
+              <option value="puzzle" className="button-create-games">
+                Puzzle
+              </option>
+              <option value="rpg" className="button-create-games">
+                RPG
+              </option>
+              <option value="simulation" className="button-create-games">
+                Simulation
+              </option>
+              <option value="sport" className="button-create-games">
+                Sport
+              </option>
+              <option value="strategie" className="button-create-games">
+                Stratégie
+              </option>
+              <option value="autre" className="button-create-games">
+                Autre
+              </option>
+            </select>
           </div>
           <div className="form-group">
             <label htmlFor="date">Date de sortie</label>
